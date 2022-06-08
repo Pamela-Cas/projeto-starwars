b4a512702a6c2c88951ac6fd760336c109164f3b
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-06-08 08:18:25 -0300

    realizando a chamada a API usando o useEffect

--- src/context/ProviderWars.js
+++ src/context/ProviderWars.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ContextWars from './ContextWars';
 
 function ProviderWars({ children }) {
+  useEffect(() => {
+    const getApiPlanets = async () => {
+      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+      const data = await response.json();
+      const dataResults = data.results;
+      console.log(dataResults);
+      return dataResults;
+    };
+    getApiPlanets();
+  }, []);
+
   return (
     <ContextWars.Provider>
       {children}
