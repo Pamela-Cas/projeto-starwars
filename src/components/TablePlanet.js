95f7f95bfaa2f1d9a15c6a7e141a033af2ead413
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2022-06-18 11:07:03 -0300

    fazendo map planet para Tabela

--- src/components/TablePlanet.js
+++ src/components/TablePlanet.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ContextWars from '../context/ContextWars';
 
 function TablePlanet() {
+  const { data } = useContext(ContextWars);
+
   return (
     <table>
       <thead>
@@ -20,6 +23,27 @@ function TablePlanet() {
           <th>URL</th>
         </tr>
       </thead>
+      <tbody>
+        { data.map((planet, index) => (
+          <tr key={ index }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>
+              {planet.films}
+            </td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 }
